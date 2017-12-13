60b208d6fbbf8c769e8def5a0fab60852b46c932
李铭昕 <715557344@qq.com>
2017-12-13 17:31:21 +0800

    分模型对应相应连接OK

--- app/tasks/Mysql/MultiTask.php
+++ app/tasks/Mysql/MultiTask.php
@@ -3,6 +3,8 @@
 namespace App\Tasks\Mysql;
 
 use App\Models\Test;
+use App\Models\Test1;
+use App\Models\Test2;
 use App\Models\User;
 use App\Tasks\Task;
 use Phalcon\Db\Adapter;
@@ -22,6 +24,12 @@ class MultiTask extends Task
         echo Color::colorize('  save          保存测试age偶数连接db2,奇数连接db', Color::FG_LIGHT_GREEN) . PHP_EOL;
         echo Color::colorize('  bsave         并发保存测试age偶数连接db2,奇数连接db', Color::FG_LIGHT_GREEN) . PHP_EOL;
         echo Color::colorize('  read          根据age分别读取数据', Color::FG_LIGHT_GREEN) . PHP_EOL;
+
+        echo Color::colorize('  save2         分模型 保存测试age偶数连接db2,奇数连接db', Color::FG_LIGHT_GREEN) . PHP_EOL;
+        echo Color::colorize('  bsave2        分模型 并发保存测试age偶数连接db2,奇数连接db', Color::FG_LIGHT_GREEN) . PHP_EOL;
+        echo Color::colorize('  read2         分模型 根据age分别读取数据', Color::FG_LIGHT_GREEN) . PHP_EOL;
+
+
     }
 
     public function getConnectionByAge($age)
@@ -30,6 +38,14 @@ class MultiTask extends Task
             return 'db2';
         }
         return 'db';
+    }
+
+    public function getConnectionIdByAge($age)
+    {
+        if ($age % 2 === 0) {
+            return '2';
+        }
+        return '';
     }
 
     public function delete()
@@ -54,6 +70,18 @@ class MultiTask extends Task
         $test->save();
     }
 
+    public function save2Action()
+    {
+        $age = rand(1, 100);
+        $id = $this->getConnectionIdByAge($age);
+        $class = '\\App\\Models\\Test' . $id;
+        /** @var Test1 $test */
+        $test = new $class();
+        $test->name = 'save2';
+        $test->age = $age;
+        $test->save();
+    }
+
     public function bsaveAction()
     {
         $age = rand(1, 100);
@@ -72,6 +100,31 @@ class MultiTask extends Task
         $test2->save();
 
         echo Color::colorize('当设置T1模型连接为db，设置T2模型连接为db2时，T1模型的连接也会被重置为db2', Color::FG_LIGHT_RED) . PHP_EOL;
+    }
+
+    public function bsave2Action()
+    {
+        $age = 99;
+        $id = $this->getConnectionIdByAge($age);
+        $class = '\\App\\Models\\Test' . $id;
+        /** @var Test1 $test */
+        $test = new $class();
+        $test->name = 'bsave2';
+        $test->age = $age;
+        $test->save();
+
+        $age = 100;
+        $id = $this->getConnectionIdByAge($age);
+        $class = '\\App\\Models\\Test' . $id;
+        /** @var Test1 $test */
+        $test2 = new $class();
+        $test2->name = 'bsave2';
+        $test2->age = $age;
+
+        $test->save();
+        $test2->save();
+
+        echo Color::success('分模型对应msyql连接OK') . PHP_EOL;
     }
 
     public function readAction()
@@ -136,5 +189,66 @@ class MultiTask extends Task
 
     }
 
+    public function read2Action()
+    {
+        $res = $this->delete();
+        echo Color::colorize('数据清理' . ($res ? '成功' : '失败'), Color::FG_LIGHT_GREEN) . PHP_EOL;
+        $sql = "INSERT INTO test(`name`,age) VALUES('read',?);";
+        $age1 = 99;
+        /** @var Adapter $db */
+        $db = di($this->getConnectionByAge($age1));
+        $db->execute($sql, [$age1]);
+
+        $age2 = 98;
+        /** @var Adapter $db */
+        $db = di($this->getConnectionByAge($age2));
+        $db->execute($sql, [$age2]);
+        echo Color::colorize('数据初始化', Color::FG_LIGHT_GREEN) . PHP_EOL;
+
+        $test1 = Test1::findFirst([
+            'conditions' => 'age = ?0',
+            'bind' => [$age1]
+        ]);
+
+        if ($test1->name == 'read' && $test1->age == $age1) {
+            echo Color::colorize('数据读取成功' . json_encode($test1->toArray()), Color::FG_LIGHT_GREEN) . PHP_EOL;
+        }
+
+        $test2 = Test2::findFirst([
+            'conditions' => 'age = ?0',
+            'bind' => [$age2]
+        ]);
+
+        if ($test2->name == 'read' && $test2->age == $age2) {
+            echo Color::colorize('数据读取成功' . json_encode($test2->toArray()), Color::FG_LIGHT_GREEN) . PHP_EOL;
+        }
+
+        $test1->name = 'write1';
+        $test2->name = 'write2';
+
+        $test1->save();
+        $test2->save();
+
+        $test1 = Test1::findFirst([
+            'conditions' => 'age = ?0',
+            'bind' => [$age1]
+        ]);
+
+        if ($test1->name == 'write1' && $test1->age == $age1) {
+            echo Color::colorize('数据写入成功' . json_encode($test1->toArray()), Color::FG_LIGHT_GREEN) . PHP_EOL;
+        }
+
+        $test2 = Test2::findFirst([
+            'conditions' => 'age = ?0',
+            'bind' => [$age2]
+        ]);
+
+        if ($test2->name == 'write2' && $test2->age == $age2) {
+            echo Color::colorize('数据写入成功' . json_encode($test2->toArray()), Color::FG_LIGHT_GREEN) . PHP_EOL;
+        } else {
+            echo Color::colorize('数据写入失败' . json_encode($test2->toArray()), Color::FG_LIGHT_RED) . PHP_EOL;
+        }
+
+    }
 }
 
